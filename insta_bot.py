587e916c0b4fc0c79bcdb4982b61348c81d37217
rashika <rashika.verma58@facebook.com>
2017-04-10 12:16:59 +0530

    modifications done

--- insta_bot.py
+++ insta_bot.py
@@ -11,14 +11,14 @@ base_url="https://api.instagram.com/v1"
 
 #function to get the owner's info using GET
 def owner_info():
-    request_url = base_url+"/users/self/?access_token="+access_token
+    request_url = base_url + "/users/self/?access_token=" + access_token
     my_info = requests.get(request_url).json()
-    print "OWNER'S NAME :"+ my_info["data"]["username"]
-    print  "OWNER'S IS :"+my_info["data"]["id"]
+    print "OWNER'S NAME :" + my_info["data"]["username"]
+    print  "OWNER'S IS :"+ my_info["data"]["id"]
 
 #funcion gets user information by using there user name
 def get_user_info_by_username(user_name):
-    user_url = base_url+"/users/search?q=="+user_name+"&access_token="+access_token
+    user_url = base_url + "/users/search?q==" + user_name + "&access_token=" + access_token
     user_info = requests.get(user_url).json()
     print user_info["data"][0]["username"]
     print user_info["data"][0]["full_name"]
@@ -28,24 +28,22 @@ def get_user_info_by_username(user_name):
 #get_user_info_by_username("badshahking143")
 
 #function to return post id by taking post_number and user_name as an argument
-def get_user_post_id(user_name,post_number):
-    i=post_number
+def get_user_post_id(user_name):
+    i = raw_input("Please enter the post number")
+    i = int(i)
     user_id = get_user_info_by_username(user_name)
-    url_id = base_url+"/users/"+user_id+"/media/recent/?access_token="+access_token
+    url_id = base_url + "/users/" + user_id + "/media/recent/?access_token=" + access_token
     rqst_url=requests.get(url_id).json()
-    if rqst_url["meta"]["code"]==200:
-        print "successfull..!!"
-    else:
-        print "invalid username or post number"
     return rqst_url["data"][i]["id"]
 
 
 
-#function calling
-#print get_user_post_id("badshahking143",0)
 
-def like_user_post(user_name,post_number):
-    media_id = get_user_post_id(user_name,post_number)
+#function calling
+#print get_user_post_id("badshahking143")
+
+def like_user_post(user_name):
+    media_id = get_user_post_id(user_name)
     token = {"access_token":access_token}
     like_url=base_url + "/media/" + media_id + "/likes"
     request_url= requests.post(like_url,token).json()
@@ -56,7 +54,7 @@ def like_user_post(user_name,post_number):
         print "OOPS..!!SOME ERROR OCCURED LIKE UNSUCCESSFULL"
         print "INVALID USERNAME OR POST NUMBER"
 
-like_user_post("badshahking143",0)
+#like_user_post("badshahking143")
 
 def post_comment(insta_username,post_number):
     media_id=get_user_post_id(insta_username,post_number)
