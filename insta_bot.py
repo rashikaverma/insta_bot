1623ba107fabaf36592e5ae859f9a527e96c8b3a
rashika <rashika.verma58@facebook.com>
2017-04-13 23:46:59 +0530

    almost a final commit

--- insta_bot.py
+++ insta_bot.py
@@ -1,13 +1,11 @@
 #importing requests lib
 import requests
 
-
 #generated access token ..!!
 access_token="2961628119.923c011.1fa38ef34c954f0a8cdfbee871bfd43a"
 
 #base url which is common to all tha urls
 base_url="https://api.instagram.com/v1"
-
 
 #function to get the owner's info using GET
 def owner_info():
@@ -16,12 +14,13 @@ def owner_info():
     print "OWNER'S NAME :" + my_info["data"]["username"]
     print  "OWNER'S ID :"+ my_info["data"]["id"]
 
-#funcion gets user information by using there user name
+#funcion returns user id which is to be used in other functions
 def get_user_id_by_username(user_name):
     user_url = base_url + "/users/search?q==" + user_name + "&access_token=" + access_token
     user_info = requests.get(user_url).json()
     return user_info["data"][0]["id"]
 
+#function to fetch all information about the user
 def get_user_info(user_name):
     user_url = base_url + "/users/search?q==" + user_name + "&access_token=" + access_token
     user_info = requests.get(user_url).json()
@@ -29,9 +28,6 @@ def get_user_info(user_name):
     print user_info["data"][0]["full_name"]
     print user_info["data"][0]["bio"]
 
-
-#function calling
-#get_user_info_by_username("badshahking143")
 
 #function to return post id by taking post_number and user_name as an argument
 def get_user_post_id(user_name):
@@ -49,9 +45,7 @@ def get_user_post_id(user_name):
     return rqst_url["data"][i]["id"]
 
 
-#function calling
-#print get_user_post_id("badshahking143")
-
+#function to like the user's post
 def like_user_post(user_name):
     media_id = get_user_post_id(user_name)
     token = {"access_token":access_token}
@@ -64,7 +58,7 @@ def like_user_post(user_name):
         print "OOPS..!!SOME ERROR OCCURED LIKE UNSUCCESSFULL"
         print "INVALID USERNAME OR POST NUMBER"
 
-#like_user_post("badshahking143")
+
 #function to post a comment on a user's post
 def post_comment(insta_username):
     media_id=get_user_post_id(insta_username)
@@ -78,14 +72,12 @@ def post_comment(insta_username):
         print "INVALID USER NAME OR POST NUMBER"
     return comment_request['data']["id"]
 
-#post_comment("badshahking143")
 
 #function to return comment id
 def comment_id(user_name):
     media_id = get_user_post_id(user_name)
     comment_id_url = base_url + "/media/" + media_id + "/comments?access_token=" + access_token
     rqst_url = requests.get(comment_id_url).json()
-    print rqst_url["data"][0]["id"]
     return rqst_url["data"][0]["id"],media_id
 
 
@@ -93,19 +85,16 @@ def comment_id(user_name):
 #function to delete your comment on user's post
 def delete_comment(user_name):
     coment_id,media_id = comment_id(user_name)
-    print coment_id
     comment_del_url = base_url + "/media/" + media_id + "/comments/"+ coment_id + "?access_token=" + access_token
     rqst_url = requests.delete(comment_del_url).json()
     done = rqst_url["meta"]["code"]
-    print done
     if done == 200:
         print "COMMENT DELETED SUCCESSFULLY"
     else:
         print "CANNOT DELETE"
 
 
-#delete_comment("apd_pankz")
-
+#function to search the comment on the user's post
 def search_comment(user_name):
     post_id=get_user_post_id(user_name)
     search=raw_input("ENTER COMMENT YOU WANT TO SEARCH")
@@ -121,8 +110,7 @@ def search_comment(user_name):
         return
 
 
-#search_comment("badshahking")
-
+#funtion to find average no of words
 def find_average_words(user_name):
     post_id = get_user_post_id(user_name)
     url = base_url + "/media/" + str(post_id) + "/comments/?access_token=" + access_token
@@ -141,14 +129,12 @@ def find_average_words(user_name):
         print "\nAverage no. of words per comment in post = %.2f" % average_words
 
 
-
-#find_average_words("badshahking")
-
-
+#main funtion to access all the funtions
 def main_fun():
     print " ~~~~~~~~~~~~~~~~~~~~~~~~~~WELCOME~~~~~~~~~~~~~~~~~~~~~"
     print "~~~~~~~OWNER INFORMATION~~~~~~~~"
     owner_info()
+    #a sub funtion to manage functioning of the other funtions defined above
     def sub_fun():
         users = ["badshahking14","apd_pankz"]
         print "PLEASE ENTER THE INSTAGRAM USER NAME"
@@ -183,15 +169,14 @@ def main_fun():
     sub_fun()
     print "DO YOU WANT TO CONTINUE ..? \n IS YES THEN PLEASE PRESS 'Y' "
     press = raw_input()
+    #while loop if user want to continue to work
     while press == 'y'or press == 'Y':
         print "DO YOU WANT TO CONTINUE ..? \n IS YES THEN PLEASE PRESS 'Y' "
         press = raw_input()
-
         sub_fun()
 
 
-
-
+#funtion calling
 main_fun()
 
 
